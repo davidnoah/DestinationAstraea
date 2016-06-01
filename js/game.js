9f38d30c7d7c3137bc7f9c46383bb41330c3d735
davidnoah <david.t.noah@gmail.com>
2016-06-01 16:56:16 -0700

    refactor

--- js/game.js
+++ js/game.js
@@ -7,30 +7,54 @@ var Game = function(canvas, context, spaceship, moon) {
 
   document.addEventListener('keyup', this.keyLetGo.bind(this));
   document.addEventListener('keydown', this.keyPressed.bind(this));
+  document.addEventListener("click", this.startPlaying.bind(this));
 };
 
 Game.prototype.draw = function() {
     var context = this.context;
     var spaceship = this.spaceship;
     var moon = this.moon;
-    context.beginPath();
-    context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    context.fillStyle = "black";
-    context.fill();
-    context.closePath();
-    context.beginPath();
-    if (spaceship.spaceship.gameOver === true) {
-      this.renderGameOver();
+      context.beginPath();
+      context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      context.fillStyle = "black";
+      context.fill();
+      context.closePath();
+
+    if (this.playing === true) {
+      context.beginPath();
+      if (spaceship.spaceship.gameOver === true) {
+        cancelAnimationFrame(this.draw.bind(this));
+        this.renderGameOver();
+      }
+      moon.drawMoon();
+      this.drawFuel();
+      spaceship.updateSpaceship();
+      spaceship.drawSpaceship();
+    } else {
+      this.displayIntro();
     }
-    moon.drawMoon();
-    this.drawFuel();
-    spaceship.updateSpaceship();
-    spaceship.drawSpaceship();
+
     requestAnimationFrame(this.draw.bind(this));
 };
 
+Game.prototype.startPlaying = function() {
+  this.playing = true;
+};
+
+Game.prototype.displayIntro = function() {
+  var context = this.context;
+  var spaceship = this.spaceship;
+  context.beginPath();
+  context.fillStyle = "rgb(224,224,224)";
+  context.textAlign = "center";
+  context.font = "30px Arial";
+  context.fillText("Destination Astraea", 500, 200);
+  context.fillText("(click to begin)", 500, 240);
+  context.closePath();
+};
+
 Game.prototype.restartPlay = function() {
-  cancelAnimationFrame(this.draw);
+  this.spaceship.spaceship.gameOver = false;
   this.draw();
 };
 
@@ -52,6 +76,9 @@ Game.prototype.renderGameOver = function() {
   context.font = "30px Arial";
   context.fillText("Game Over! You Lose", 500, 200);
   context.closePath();
+
+  document.removeEventListener("click", this.startPlaying.bind(this));
+  document.addEventListener("click", this.restartPlay.bind(this));
 };
 
 Game.prototype.keyLetGo = function(event) {
