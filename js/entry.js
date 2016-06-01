ede4ea73ba77ed264213e8e0bca27e4aeb49a574
davidnoah <david.t.noah@gmail.com>
2016-06-01 12:40:53 -0700

    finsih refactor

--- js/entry.js
+++ js/entry.js
@@ -7,39 +7,9 @@ var Moon = require('./moon.js');
 function startGame() {
   var moon = new Moon(context);
   var spaceship = new Spaceship(context, moon);
-  var game = new Game(context, spaceship, moon);
+  var game = new Game(canvas, context, spaceship, moon);
 
   game.draw();
 }
-
-function keyLetGo(event) {
-    switch(event.keyCode) {
-        case 37:
-            spaceship.spaceship.rotatingLeft = false;
-            break;
-        case 39:
-            spaceship.spaceship.rotatingRight = false;
-            break;
-        case 38:
-            spaceship.spaceship.engineOn = false;
-            break;
-    }
-}
-document.addEventListener('keyup', keyLetGo);
-
-function keyPressed(event) {
-    switch(event.keyCode) {
-        case 37:
-            spaceship.spaceship.rotatingLeft = true;
-            break;
-        case 39:
-            spaceship.spaceship.rotatingRight = true;
-            break;
-        case 38:
-            spaceship.spaceship.engineOn = true;
-            break;
-    }
-}
-document.addEventListener('keydown', keyPressed);
 
 startGame();
--- js/game.js
+++ js/game.js
@@ -1,32 +1,37 @@
-var Game = function(context, spaceship, moon) {
+var Game = function(canvas, context, spaceship, moon) {
+  this.canvas = canvas;
   this.context = context;
   this.playing = false;
   this.moon = moon;
   this.spaceship = spaceship;
+
+  document.addEventListener('keyup', this.keyLetGo.bind(this));
+  document.addEventListener('keydown', this.keyPressed.bind(this));
 };
 
 Game.prototype.draw = function() {
     var context = this.context;
     var spaceship = this.spaceship;
+    var moon = this.moon;
     context.beginPath();
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     context.fillStyle = "black";
     context.fill();
     context.closePath();
     context.beginPath();
     if (spaceship.spaceship.gameOver === true) {
-      renderGameOver();
+      this.renderGameOver();
     }
     moon.drawMoon();
-    drawFuel();
+    this.drawFuel();
     spaceship.updateSpaceship();
     spaceship.drawSpaceship();
-    requestAnimationFrame(draw);
+    requestAnimationFrame(this.draw.bind(this));
 };
 
 Game.prototype.restartPlay = function() {
-  cancelAnimationFrame(draw);
-  draw();
+  cancelAnimationFrame(this.draw);
+  this.draw();
 };
 
 Game.prototype.drawFuel = function() {
@@ -40,12 +45,43 @@ Game.prototype.drawFuel = function() {
 
 Game.prototype.renderGameOver = function() {
   var context = this.context;
+  context.save();
   context.beginPath();
   context.fillStyle = "rgb(224,224,224)";
   context.textAlign = "center";
   context.font = "30px Arial";
   context.fillText("Game Over! You Lose", 500, 200);
   context.closePath();
+};
+
+Game.prototype.keyLetGo = function(event) {
+  var spaceship = this.spaceship;
+  switch(event.keyCode) {
+      case 37:
+          spaceship.spaceship.rotatingLeft = false;
+          break;
+      case 39:
+          spaceship.spaceship.rotatingRight = false;
+          break;
+      case 38:
+          spaceship.spaceship.engineOn = false;
+          break;
+  }
+};
+
+Game.prototype.keyPressed = function(event) {
+  var spaceship = this.spaceship;
+  switch(event.keyCode) {
+      case 37:
+          spaceship.spaceship.rotatingLeft = true;
+          break;
+      case 39:
+          spaceship.spaceship.rotatingRight = true;
+          break;
+      case 38:
+          spaceship.spaceship.engineOn = true;
+          break;
+    }
 };
 
 module.exports = Game;
